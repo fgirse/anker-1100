c3e08cd1f3f83504388aa36f04eb9332d9bcbc15
fgirse <fgirs@bluewin.ch>
2025-11-15 14:04:48 +0100

    implement team side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import BentoGrid from "../components/Bento";
 import Sportarena from "../components/Sportarena";
+import Team from "../components/Team";
 export default function Home() {
   return (
     <div className="container bg-black flex flex-col min-h-screen items-center justify-center  font-sans dark:bg-black">
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <BentoGrid />
       <Sportarena />
+      <Team />
     </div>
   );
 }
--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -25,54 +25,54 @@ export default function Bento() {
 
 md:gap-2 m-4">
       <div className="hero col-start-1 row-start-1 row-span-2  bg-slate-800 gap-5 md:col-start-1 md:row-start-1 md:col-span-4 md:row-span-1 lg:bg-slate-900 rounded-md p-10">
-        <h1 className=" lg:top-[2vh] text-center top-12 text-4xl headingA text-black lg:text-6xl">unser angebot und unser service</h1>
+        <h1 className=" text-center text-4xl headingA text-black lg:text-6xl">unser angebot und unser service</h1>
       </div>
-      <div className="lg:relative col-start-1 row-start-3 row-span-2 md:col-start-5 md:row-start-1 md:col-span-2 md:row-span-2 lg:bg-slate-800 rounded-md p-10">
-        <div className="relative top-9  lg:top-[5vh] lg:w-screen lg:h-screen flex flex-col items-center ">
+      <div className=" col-start-1 row-start-3 row-span-1 md:col-start-5 md:row-start-1 md:col-span-2 md:row-span-2 lg:bg-slate-800 rounded-md p-10">
+        <div className=" lg:top-[5vh] lg:w-screen lg:h-screen flex flex-col items-center ">
           <Image src={LogoNeu} alt="Logo Neu" width={800} className="border-2 border-white" />
         </div>
       </div>
-      <div className="col-start-1 row-start-5 row-span-3   md:col-start-1 md:row-start-2 md:col-span-2 md:row-span-3 bg-red-900 rounded-md p-10">
-        <div className="relative top-12 lg:relative lg:top-[5vh] lg:left-[4vw] ">
+      <div className="football col-start-1 row-start-4 row-span-1   md:col-start-1 md:row-start-2 md:col-span-2 md:row-span-3 bg-red-900 rounded-md p-10">
+        <div className="lg:relative lg:top-[5vh] lg:left-[4vw] ">
           <Image src={Astra} alt="Astra vom Fass" width={500} height={200} />
         </div>
       </div>
-      <div className="football col-start-1 row-start-8 row-span-2 md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-3 bg-red-900 rounded-md p-10">
-<h1 className=" top-7 lg:relative lg:top-[4vh] text-4xl text-center headingA text-yellow-600 lg:text-4xl">bier vom fass</h1>
+      <div className="football col-start-1 row-start-5 row-span-1 md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-3 bg-red-900 rounded-md p-10">
+<h1 className=" text-4xl text-center headingA text-yellow-600 lg:text-4xl">bier vom fass</h1>
 
           <h2 className=" text-2xl text-center text-white lg:text-2xl lg:relative lg:top-[8vh]">
-            Flensburger Pils - das kühle, frische Blonde von der Waterkant. Astra-Pils - das Kultbier natürlich direkt vom Kiez
+            Flensburger Pils - das kühle, frische Blonde von der Waterkant. Astra-Pils - das berühmte Kultbier natürlich direkt vom Kiez
           </h2>
       </div>
-      <div className="col-start-1 row-start-10 row-span-2  md:col-start-4 md:row-start-3 md:col-span-2 md:row-span-3 bg-yellow-800 rounded-md p-10">
+      <div className="col-start-1 row-start-6 row-span-1  md:col-start-4 md:row-start-3 md:col-span-2 md:row-span-3 bg-yellow-800 rounded-md p-10">
           <div className="lg:relative lg:top-[5vh] flex flex-col items-center ">
             <Image src={Wine} alt="Wine Illustration" width={500} height={300} />
           </div>
 
       </div>
-      <div className="football col-start-1 row-start-12 row-span-6 md:col-start-6 md:row-start-3 md:col-span-1 md:row-span-3 bg-yellow-800 rounded-md p-10">
+      <div className="football col-start-1 row-start-7 row-span-1 md:col-start-6 md:row-start-3 md:col-span-1 md:row-span-3 bg-yellow-800 rounded-md p-10">
      <h1 className=" lg:top-[5vh] text-4xl text-center headingA text-yellow-600 lg:text-4xl">regionale weine</h1>
             <h2 className=" top-5 lg:top-[8vh] text-center text-2xl text-white lg:text-2xl">
               Qualitativ hochwertige Weine aus der Region Kaiserstuhl und dem Markgräflerland. Hauslieferant Weingut Heinemann Scherzingen
             </h2>
 
       </div>
-      <div className="col-start-1 row-start-18 row-span-2   md:col-start-1 md:row-start-5 md:col-span-1 md:row-span-3 bg-stone-800 rounded-md p-10">
-         <div className="relative left-20 lg:top-[2vh] lg:left-[8vw] lg:w-[60vw] lg:h-[60vh] justify-center">
-            <Image src={Cocktail} alt="CocktailIcon" width={200}/>
+      <div className="col-start-1 row-start-8 row-span-2   md:col-start-1 md:row-start-5 md:col-span-1 md:row-span-3 bg-stone-800 rounded-md p-10">
+         <div className="lg:w-[60vw] lg:h-[60vh] justify-center">
+            <Image src={Cocktail} alt="CocktailIcon" width={200} height={200}/>
           </div>
 
       </div>
-      <div className="football col-start-1 row-start-20 row-span-8 md:col-start-2 md:row-start-5 md:col-span-2 md:row-span-3 bg-stone-800 rounded-md p-10">
+      <div className="football col-start-1 row-start-10 row-span-1 md:col-start-2 md:row-start-5 md:col-span-2 md:row-span-3 bg-stone-800 rounded-md p-10">
 
-<h1 className=" lg:top-[3vh] text-3xl text-center headingA text-yellow-600 lg:text-3xl">cocktails & longdrinks</h1>
+<h1 className=" text-3xl text-center headingA text-yellow-600 lg:text-3xl">cocktails & longdrinks</h1>
           <h2 className=" lg:top-[5vh] text-xl text-center text-white lg:text-[1.4rem]">
             Erleben Sie unsere kreativen Cocktails und erfrischenden Longdrinks, die mit hochwertigen Spirituosen und frischen Zutaten köstlich zubereitet werden.
           </h2>
 
 
       </div>
-      <div className="football col-start-1 row-start-28 row-span-6 md:col-start-4 md:row-start-7 md:col-span-2 md:row-span-2 bg-red-900 rounded-md p-10">
+      <div className="football col-start-1 row-start-11 row-span-1 md:col-start-4 md:row-start-7 md:col-span-2 md:row-span-2 bg-red-900 rounded-md p-10">
 
           <h1 className=" top-3 lg:top-[5vh] text-center text-4xl headingA text-yellow-600 lg:text-5xl">fussball tv live</h1>
           <h2 className="text-[1.3rem] text-center text-white lg:text-[1.1rem] top-2 lg:top-[5vh]">
@@ -82,47 +82,46 @@ md:gap-2 m-4">
           
 
       </div>
-      <div className="col-start-1 row-start-34 row-span-6  md:col-start-6 md:row-start-6 md:col-span-1 md:row-span-3 bg-red-900 rounded-md p-10">
-        <div className="lg:relative lg:top-[8vh] flex flex-col items-center justify-center">
+      <div className="footballcol-start-1 row-start-12 row-span-1  md:col-start-6 md:row-start-6 md:col-span-1 md:row-span-3 bg-red-900 rounded-md p-10">
+        <div className=" flex flex-col items-center justify-center">
             <Image src={Fussball} alt="Fussball-Illustration" width={175} height={175} />
           </div>
         
        
       </div>
-      <div className="col-start-1 row-start-40 row-span-4   md:col-start-1 md:row-start-8 md:col-span-2 md:row-span-1 bg-slate-800 rounded-md p-10">
-        <div className="relative lg:top-[5vh] flex flex-col items-center justify-center">
+      <div className="col-start-1 row-start-13 row-span-1   md:col-start-1 md:row-start-8 md:col-span-2 md:row-span-1 bg-slate-800 rounded-md p-10">
+        <div className="flex flex-col items-center justify-center">
           <Image src={LogoAlt} alt="Logo Alt" width={400} height={200} />
         </div>
       </div>
-      <div className="col-start-1 row-start-43    md:col-start-3 md:row-start-8 md:col-span-3 md:row-span-1 bg-slate-800 rounded-md p-10">
+      <div className="col-start-1 row-start-14 md:col-start-3 md:row-start-8 md:col-span-3 md:row-span-1 bg-slate-800 rounded-md p-10">
         
         </div>
-      <div className="football col-start-1 row-start-44 row-span-6   md:col-start-1 md:row-start-9 md:col-span-2 md:row-span-3 bg-yellow-800 rounded-md p-10">
+      <div className="football col-start-1 row-start-15 row-span-1   md:col-start-1 md:row-start-9 md:col-span-2 md:row-span-3 bg-yellow-800 rounded-md p-10">
 
-                 <h1 className="relative top-3 lg:top-[5vh] text-4xl text-center headingA text-yellow-500 lg:text-4xl">party - event ?</h1>
+                 <h1 className=" text-4xl text-center headingA text-yellow-500 lg:text-4xl">party - event ?</h1>
           <h2 className="relative top-3 text-xl text-center text-white lg:text-2xl lg:top-[8vh]">
-            Der Rettungsanker ist die ideale Location für Ihren privaten oder Business Event. Im Rahmen einer "gesckossenen Gesellschaft" stehen Ihnen die Räumlichkeiten des Rettungsankers zur Verfügung. Auf Wunsch Catering durch unseren Kooperationspartner möglich ! Sprechen Sie uns an oder kontaktieren Sie uns per e.mail.
-          </h2> 
-                  
-         
+            Der Rettungsanker ist die ideale Location für Ihren privaten oder Business Event. Im Rahmen einer "geschlossenen Gesellschaft" stehen Ihnen die Räumlichkeiten des Rettungsankers zur Verfügung. Auf Wunsch Catering durch unseren Kooperationspartner möglich ! Sprechen Sie uns an oder kontaktieren Sie uns per e.mail.
+          </h2>
+
         </div>
-      <div className="col-start-1 row-start-50 row-span-4  md:col-start-3 md:row-start-9 md:col-span-1 md:row-span-3 bg-yellow-800 rounded-md p-10">
-       <div className="lg:relative lg:top-[5vh] flex flex-col items-center  justify-center">
+      <div className="col-start-1 row-start-16 row-span-1  md:col-start-3 md:row-start-9 md:col-span-1 md:row-span-3 bg-yellow-800 rounded-md p-10">
+       <div className="flex flex-col items-center  justify-center">
 
 
             <Image src={Crowdparty} alt="Crowd Illustration" width={600} height={200} />
           </div>
       </div>
-      <div className="football col-start-1 row-start-54 row-span-5   md:col-start-4 md:row-start-9 md:col-span-2 md:row-span-3 bg-stone-600 rounded-md p-10">
+      <div className="football col-start-1 row-start-17 row-span-1   md:col-start-4 md:row-start-9 md:col-span-2 md:row-span-3 bg-stone-600 rounded-md p-10">
 
-          <h1 className="lg:relative lg:top-[5vh] text-5xl text-center headingA text-yellow-600 lg:text-3xl">hans albers</h1>
+          <h1 className="text-5xl text-center headingA text-yellow-600 lg:text-3xl">hans albers</h1>
           <h2 className="text-xl text-center text-white lg:text-2xl lg:relative lg:top-[8vh]  ">
             Hans Phillip August Albers (* 22.September 1891 in Hamburg , 24. Juli 1960 in Berg, Bayern) war ein deutscher Schauspieler und Sänger, der als "blonder Hans" Volkssidol wurde. Zu den bekanntesten Spielfilmen in denen er mitwirkte gehören "der Mann, der Sherlock Holmes war" (1937), "Münchhausen" (1943), "die grosse Freiheit Nr.7" (1943) sowie "Auf fer Reeperbahn Nachts um halb eins
           </h2>
 
 
       </div>
-      <div className="col-start-1 row-start-59 row-span-5   md:col-start-6 md:row-start-9 md:col-span-1 md:row-span-3 bg-stone-600 rounded-md p-10">
+      <div className="col-start-1 row-start-18 row-span-5   md:col-start-6 md:row-start-9 md:col-span-1 md:row-span-3 bg-stone-600 rounded-md p-10">
         <div className="w-80 flex flex-col items-center relative top-[1vh]">
             <Image src={HansAlbers} alt="Hans Albers" width={275} height={275} />
           </div>
